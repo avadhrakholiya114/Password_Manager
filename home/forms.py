f2dcaa567426ea45a17403ce7f8e35530c899b36
avadh <avadhrakholiya114@gmail.com>
2023-10-07 10:23:28 +0530

    Registration

---
+++ home/forms.py
@@ -0,0 +1,33 @@
+from  django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class RegitrationForm(UserCreationForm):
+    # Form to create new user
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Password'}
+        ),
+    )
+    password2 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}
+        ),
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email',)
+        widgets = {
+            'username': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Username'}
+            ),
+            'email': forms.EmailInput(
+                attrs={'class': 'form-control', 'placeholder': 'Email'}
+            )
+        }
+
+
--- home/views.py
+++ home/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import RegitrationForm
+from django.contrib import messages
+
+# password
 def index(request):
     return render(request,"home.html")
+
+def register(request):
+    if request.method == "POST":
+        form = RegitrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Account created successfully. Please login to your account')
+
+    else:
+        form = RegitrationForm()
+    return render(request, 'signup.html', {'form': form})
